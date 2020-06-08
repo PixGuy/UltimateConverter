d0c37159b6c61cffafbd4482a52d2401207d7804
PixGuy <57600297+PixGuy@users.noreply.github.com>
2020-06-08 10:00:17 -0300

    Update 3.5
    Changelog:
    Fixed bugs

--- Advancedmode.lua
+++ Advancedmode.lua
@@ -2,15 +2,12 @@ print("Advanced mode above, if you don't want to use it type 'null' ")
 local answer = io.read()
 if answer == "null" then io.close() else if answer == "proceed" then 
     print("Advanced mode enabled! To use it you need to know what you are doing W.I.P")
-    print("Choose a method: Reverse, Normal")
+    print("Choose a method: Reverse, W.I.P")
     local option = io.read()
     if option == "reverse" then print("enter a message")
-    else if option == "normal" then
-        print("To use normal use the file named Lua oh lua.lua")
     else io.close()
     end
     local madame = io.read()
     print(string.reverse(madame))
-end
 end
 end

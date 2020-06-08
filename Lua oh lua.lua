f2dc3d5710c7ddd3c7d98a913a0dee36aebb463e
PixGuy <57600297+PixGuy@users.noreply.github.com>
2020-06-08 09:04:48 -0300

    Update 3.0
    Changelog:
    Removed Advanced Mode From Lua Oh Lua.lua
    New File For Advanced Mode 
    Advanced Mode Is Still A W.I.P But You Can Use It

--- Lua oh lua.lua
+++ Lua oh lua.lua
@@ -6,9 +6,3 @@ local num1 = io.read()
 local num2 = io.read()
 print(string.byte(message,num1,num2))
 --This code is made by PixGuy any clones will be reported and removed--
-print("Advanced mode above, if you don't want to use it type 'null' ")
-local answer = io.read()
-if answer == "null" then io.close() else if answer == "proceed" then 
-    print("Advanced mode enabled! To use it you need to know what you are doing W.I.P")
-end
-end
